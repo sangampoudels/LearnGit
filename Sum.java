676e7f7b73922e661810511df3bf21dd5b18f637
sangam <poudelsangam05@gmail.com>
2022-04-20 08:26:12 +0545

    adding Diff.java

--- Sum.java
+++ Sum.java
@@ -2,6 +2,6 @@ public class sum{
     public ststic void main(String args[]){
         int a=4;
         int b=5;
-        system.out.println(a=b);
+        system.out.println("The sum is "+(a+b));
     }
 }
